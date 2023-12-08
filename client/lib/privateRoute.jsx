009864fe0ba1e9330619e0dec4581f806a432841
“pineapple” <alexnemoto@Alexs-MacBook-Pro.local>
2023-12-07 22:39:30 -0500

    update filename

--- client/lib/privateRoute.jsx
+++ client/lib/privateRoute.jsx
@@ -1,9 +1,9 @@
 // import React from "react";
-import {  Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import auth from "./authHelper";
 import PropTypes from "prop-types";
 
-export default function PrivateRoute ({ children, ...rest }) {
+export default function PrivateRoute({ children, ...rest }) {
   const location = useLocation();
 
   return auth.isAuthenticated() ? (
